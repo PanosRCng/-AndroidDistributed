3ad2187a381715c856f1cdfd042c8d6ff2284eed
PanosRCng <panosracing@homtail.com>
2013-04-22 02:35:05 +0300

    cleaning

---
+++ dynamix/dynamix_to_start/BatteryLevelPlugin-1.0.0/BatteryLevelPlugin/gen/org/ambientdynamix/contextplugins/batterylevel/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.ambientdynamix.contextplugins.batterylevel;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
