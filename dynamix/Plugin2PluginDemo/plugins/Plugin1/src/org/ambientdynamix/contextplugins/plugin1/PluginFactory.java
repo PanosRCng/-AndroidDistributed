369c8840e003e4fe6b5072b1e7e3d7ba2cfee9d4
PanosRCng <panosracing@homtail.com>
2013-07-14 13:41:54 +0300

    cleaning

--- dynamix/Plugin2PluginDemo/plugins/Plugin1/src/org/ambientdynamix/contextplugins/plugin1/PluginFactory.java
+++
@@ -1,9 +0,0 @@
-package org.ambientdynamix.contextplugins.plugin1;
-
-import org.ambientdynamix.api.contextplugin.ContextPluginRuntimeFactory;
-
-public class PluginFactory extends ContextPluginRuntimeFactory {
-	public PluginFactory() {
-		super(Plugin1Runtime.class, null, null);
-	}
-}
--- dynamix/Plugin2PluginDemo/plugins/Plugin2/src/org/ambientdynamix/contextplugins/plugin2/PluginFactory.java
+++
@@ -1,9 +0,0 @@
-package org.ambientdynamix.contextplugins.plugin2;
-
-import org.ambientdynamix.api.contextplugin.ContextPluginRuntimeFactory;
-
-public class PluginFactory extends ContextPluginRuntimeFactory {
-	public PluginFactory() {
-		super(Plugin2Runtime.class, null, null);
-	}
-}
